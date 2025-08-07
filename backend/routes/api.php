06fdd1ad3b9ddb305e24c5462f5f6e75cb9cb9f0
hamzakhiar <khiarihamza99@gmail.com>
2025-08-07 13:37:59 -0700

    define routes done

---
+++ backend/routes/api.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\DemandeAchatController;
+use App\Http\Controllers\MagasinController;
+use App\Http\Controllers\MouvementController;
+use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\StockMagasinController;
+use App\Http\Controllers\Api\TransfertController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Public authentication routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // Authentication
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // User management
+    Route::apiResource('users', UserController::class);
+    
+    // Core resources
+    Route::apiResource('produits', ProduitController::class);
+    Route::apiResource('magasins', MagasinController::class);
+    Route::apiResource('demandes-achat', DemandeAchatController::class);
+    Route::apiResource('mouvements', MouvementController::class);
+    
+    // Stock management
+    Route::prefix('stocks')->group(function () {
+        Route::get('/', [StockMagasinController::class, 'index']);
+        Route::post('/', [StockMagasinController::class, 'store']);
+        Route::get('/{id}', [StockMagasinController::class, 'show']);
+        Route::put('/{id}', [StockMagasinController::class, 'update']);
+        Route::delete('/{id}', [StockMagasinController::class, 'destroy']);
+    });
+    
+    // Transfer management
+    Route::prefix('transferts')->group(function () {
+        Route::get('/', [TransfertController::class, 'index']);
+        Route::post('/', [TransfertController::class, 'store']);
+        Route::get('/{id}', [TransfertController::class, 'show']);
+        Route::put('/{id}', [TransfertController::class, 'update']);
+        Route::delete('/{id}', [TransfertController::class, 'destroy']);
+    });
+});
